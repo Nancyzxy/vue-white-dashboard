d32026d212822d1102ce52ec6b456405e0105057
Nancyzxy <11912039@mail.sustech.edu.cn>
2022-05-20 19:46:34 +0800

    commit ContentFooter.vue

--- src/pages/Layout/ContentFooter.vue
+++ src/pages/Layout/ContentFooter.vue
@@ -4,29 +4,17 @@
       <ul class="nav">
         <li class="nav-item">
           <a href="javascript:void(0)" class="nav-link">
-            Creative Tim
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="javascript:void(0)" class="nav-link">
-            About Us
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="javascript:void(0)" class="nav-link">
-            Blog
+            11912039 , 11911716
           </a>
         </li>
       </ul>
       <div class="copyright">
         &copy;
-        {{ new Date().getFullYear() }} made with <i class="tim-icons icon-heart-2"></i> by
-        <a href="javascript:void(0)" target="_blank">Creative Tim</a> for a better web.
+        {{ new Date().getFullYear() }}
+        Made as <a href="javascript:void(0)" target="_blank">CS209</a> Project
       </div>
     </div>
   </footer>
 </template>
-<script>
-</script>
-<style>
-</style>
+<script></script>
+<style></style>
