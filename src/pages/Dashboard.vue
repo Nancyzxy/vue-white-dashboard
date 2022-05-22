74d866ca7e3bd96ac9c4e010a82c0970b2160e80
Nancyzxy <11912039@mail.sustech.edu.cn>
2022-05-22 20:52:17 +0800

    基本完成主页面 正负柱状图还没画

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,10 +7,17 @@
             <div class="row">
               <div class="col-sm-6" :class="'text-left'">
                 <template>
-                  <h5 class="card-category">Total Shipments</h5>
+                  <h3 class="card-title">
+                    Total Questions : {{ questionNum }}
+                  </h3>
                 </template>
                 <template>
-                  <h2 class="card-title">Performance</h2>
+                  <h3 class="card-title">Total Tags : {{ tagNum }}</h3>
+                </template>
+                <template>
+                  <el-tag>
+                    {{ descrption[BigBarGraph.activeIndex] }}
+                  </el-tag>
                 </template>
               </div>
               <div class="col-sm-6">
@@ -21,10 +28,10 @@
                 >
                   <template>
                     <label
-                      v-for="(option, index) in bigLineChartCategories"
+                      v-for="(option, index) in BigBarGraphCategories"
                       :key="option"
                       class="btn btn-success btn-sm btn-simple"
-                      :class="{ active: bigLineChart.activeIndex === index }"
+                      :class="{ active: BigBarGraph.activeIndex === index }"
                       :id="index"
                     >
                       <input
@@ -32,7 +39,7 @@
                         @click="initBigChart(index)"
                         name="options"
                         autocomplete="off"
-                        :checked="bigLineChart.activeIndex === index"
+                        :checked="BigBarGraph.activeIndex === index"
                       />
                       {{ option }}
                     </label>
@@ -42,117 +49,41 @@
             </div>
           </template>
           <!-- chart的内容 -->
-          <line-chart
+          <bar-chart
             class="chart-area"
             ref="bigChart"
             chart-id="big-line-chart"
-            :chart-data="bigLineChart.chartData"
-            :gradient-colors="bigLineChart.gradientColors"
-            :gradient-stops="bigLineChart.gradientStops"
-            :extra-options="bigLineChart.extraOptions"
-          >
-          </line-chart>
-        </card>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-4" :class="'text-left'">
-        <card type="chart" cardCol>
-          <template slot="header">
-            <h5 class="card-category">{{ $t("dashboard.totalShipments") }}</h5>
-            <h3 class="card-title">
-              <i class="tim-icons icon-bell-55 text-primary "></i> 763,215
-            </h3>
-          </template>
-          <line-chart
-            class="chart-area"
-            chart-id="green-line-chart"
-            :chart-data="greenLineChart.chartData"
-            :gradient-colors="greenLineChart.gradientColors"
-            :gradient-stops="greenLineChart.gradientStops"
-            :extra-options="greenLineChart.extraOptions"
-          >
-          </line-chart>
-        </card>
-      </div>
-      <div class="col-lg-4">
-        <card type="chart" cardCol>
-          <template slot="header">
-            <h5 class="card-category">{{ $t("dashboard.dailySales") }}</h5>
-            <h3 class="card-title">
-              <i class="tim-icons icon-delivery-fast text-info "></i> 3,500€
-            </h3>
-          </template>
-          <bar-chart
-            class="chart-area"
-            chart-id="blue-bar-chart"
-            :chart-data="blueBarChart.chartData"
-            :gradient-stops="blueBarChart.gradientStops"
-            :extra-options="blueBarChart.extraOptions"
+            :chart-data="BigBarGraph.chartData"
+            :gradient-stops="BigBarGraph.gradientStops"
+            :extra-options="BigBarGraph.extraOptions"
           >
           </bar-chart>
         </card>
       </div>
-      <div class="col-lg-4">
-        <card type="chart" cardCol>
-          <template slot="header">
-            <h5 class="card-category">{{ $t("dashboard.completedTasks") }}</h5>
-            <h3 class="card-title">
-              <i class="tim-icons icon-send text-success "></i> 12,100K
-            </h3>
-          </template>
-          <line-chart
-            class="chart-area"
-            chart-id="purple-line-chart"
-            :chart-data="purpleLineChart.chartData"
-            :gradient-stops="purpleLineChart.gradientStops"
-            :extra-options="purpleLineChart.extraOptions"
-          >
-          </line-chart>
-        </card>
-      </div>
     </div>
     <div class="row">
-      <div class="col-lg-6 col-md-12">
-        <card type="tasks">
+      <div class="col-lg-6 col-md-12" :class="'text-left'">
+        <card type="chart" cardCol>
           <template slot="header">
-            <template>
-              <h6 class="title d-inline">Tasks(5)</h6>
-            </template>
-            <template>
-              <p class="card-category d-inline">Today</p>
-            </template>
-            <drop-down tag="div" :class="''">
-              <button
-                aria-label="Settings menu"
-                data-toggle="dropdown"
-                class="dropdown-toggle btn-rotate btn btn-link btn-icon"
-                :class="''"
-              >
-                <i class="tim-icons icon-settings-gear-63"></i>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-right">
-                <a href="#pablo" class="dropdown-item">Action</a>
-                <a href="#pablo" class="dropdown-item">Another Action</a>
-                <a href="#pablo" class="dropdown-item">Something else</a>
-              </ul>
-            </drop-down>
+            <h3 class="card-title">
+              Percentage of answered questions
+            </h3>
           </template>
-          <div class="table-full-width table-responsive">
-            <task-list></task-list>
-          </div>
+          <el-row>
+            <div id="chartPie" style="width:100%; height:400px;"></div>
+          </el-row>
         </card>
       </div>
       <div class="col-lg-6 col-md-12">
-        <card class="card">
-          <h4 slot="header" class="card-title">
-            <template>
-              Simple Table
-            </template>
-          </h4>
-          <div class="table-responsive">
-            <user-table></user-table>
-          </div>
+        <card type="chart" cardCol>
+          <template slot="header">
+            <h3 class="card-title">
+              Percentage of positive scored questions
+            </h3>
+          </template>
+          <el-row>
+            <div id="chartPie_" style="width:100%; height:400px;"></div>
+          </el-row>
         </card>
       </div>
     </div>
@@ -160,117 +91,126 @@
 </template>
 <script>
 import { Card } from "@/components/index";
-
-import LineChart from "@/components/Charts/LineChart";
+import echarts from "echarts";
 import BarChart from "@/components/Charts/BarChart";
 import * as chartConfigs from "@/components/Charts/config";
-import TaskList from "./Dashboard/TaskList";
-import UserTable from "./Dashboard/UserTable";
 import config from "@/config";
+import axios from "axios";
 
 export default {
   components: {
     Card,
-    LineChart,
-    BarChart,
-    TaskList,
-    UserTable
+    BarChart
   },
   data() {
     return {
-      bigLineChartCategories: ["Accounts", "Purchases", "Sessions"],
-      bigLineChart: {
-        allData: [
-          [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-          [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-          [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
-        ],
+      chartPie: null,
+      answer: 0,
+      notAnswered: 0,
+      positiveScored: 0,
+      negativeScored: 0,
+      questionNum: 0,
+      tagNum: 0,
+      chartLabels: [],
+      BigBarGraphCategories: [
+        "QuestionCount",
+        "ViewCount",
+        "AnswerCount",
+        "Score"
+      ],
+      descrption: [
+        "This chart describes the number of questions the top 10 tags have.",
+        "This chart describes the times the top 10 tags have been watched.",
+        "This chart describes the number of answers the top 10 tags' questions have.",
+        "This chart describes the highest and lowest scores of the top 10 tags' questions."
+      ],
+      BigBarGraph: {
+        allData: [[], [], [], []],
         activeIndex: 0,
         chartData: { datasets: [{}] },
-        extraOptions: chartConfigs.purpleChartOptions,
-        gradientColors: config.colors.primaryGradient,
+        extraOptions: chartConfigs.barChartOptions,
         gradientStops: [1, 0.4, 0],
         categories: []
-      },
-      greenLineChart: {
-        extraOptions: chartConfigs.greenChartOptions,
-        chartData: {
-          labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
-          datasets: [
-            {
-              label: "Data",
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: "rgba(255,255,255,0)",
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.2, 0]
-      },
-      blueBarChart: {
-        extraOptions: chartConfigs.barChartOptions,
-        chartData: {
-          labels: ["USA", "GER", "AUS", "UK", "RO", "BR"],
-          datasets: [
-            {
-              label: "Countries",
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.4, 0]
-      },
-      purpleLineChart: {
-        extraOptions: chartConfigs.purpleChartOptions,
-        chartData: {
-          labels: ["JUL", "AUG", "SEP", "OCT", "NOV"],
-          datasets: [
-            {
-              label: "My First dataset",
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: "rgba(255,255,255,0)",
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80]
-            }
-          ]
-        },
-        gradientColors: [
-          "rgba(66,134,121,0.15)",
-          "rgba(66,134,121,0.0)",
-          "rgba(66,134,121,0)"
-        ],
-        gradientStops: [1, 0.4, 0]
       }
     };
   },
   computed: {},
   methods: {
+    getQuestionCount() {
+      axios
+        .get("/getQuestionCount")
+        .then(res => {
+          this.questionNum = res.data;
+          this.getAnswered();
+          this.getScore();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    getAnswered() {
+      axios
+        .get("/getAnswerCountLarger0")
+        .then(res => {
+          this.answer = res.data;
+          this.notAnswered = this.questionNum - this.answer;
+          this.drawPieChart();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    drawPieChart() {
+      this.chartPie = echarts.init(document.getElementById("chartPie"));
+      this.chartPie.setOption({
+        title: {},
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "right",
+          data: ["answered", "not answered"]
+        },
+        series: [
+          {
+            name: "questions that are",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: this.answer, name: "answered" },
+              { value: this.notAnswered, name: "not answered" }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      });
+    },
+    getTop10Tags() {
+      axios
+        .get("/getTopTags/10")
+        .then(res => {
+          let tags = res.data;
+          for (let i = 0; i < tags.length; i++) {
+            this.chartLabels.push(tags[i].name);
+            this.BigBarGraph.allData[0].push(tags[i].question_cnt);
+            this.BigBarGraph.allData[1].push(tags[i].view_count);
+            this.BigBarGraph.allData[2].push(tags[i].answer_count);
+          }
+          this.initBigChart(0);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
     initBigChart(index) {
       let chartData = {
         datasets: [
@@ -287,36 +227,78 @@ export default {
             pointHoverRadius: 4,
             pointHoverBorderWidth: 15,
             pointRadius: 4,
-            data: this.bigLineChart.allData[index]
+            data: this.BigBarGraph.allData[index]
           }
         ],
-        labels: [
-          "JAN",
-          "FEB",
-          "MAR",
-          "APR",
-          "MAY",
-          "JUN",
-          "JUL",
-          "AUG",
-          "SEP",
-          "OCT",
-          "NOV",
-          "DEC"
-        ]
+        labels: this.chartLabels
       };
       this.$refs.bigChart.updateGradients(chartData);
-      this.bigLineChart.chartData = chartData;
-      this.bigLineChart.activeIndex = index;
+      this.BigBarGraph.chartData = chartData;
+      this.BigBarGraph.activeIndex = index;
+    },
+    getTagCount() {
+      axios
+        .get("/getTagCount")
+        .then(res => {
+          this.tagNum = res.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    getScore() {
+      axios
+        .get("/getScoreCountLarger0")
+        .then(res => {
+          this.positiveScored = res.data;
+          this.negativeScored = this.questionNum - this.positiveScored;
+          this.drawPieChart_();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    drawPieChart_() {
+      this.chartPie = echarts.init(document.getElementById("chartPie_"));
+      this.chartPie.setOption({
+        title: {},
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "right",
+          data: ["positiveScored", "negativeScored"]
+        },
+        series: [
+          {
+            name: "questions that are",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: this.positiveScored, name: "positiveScored" },
+              { value: this.negativeScored, name: "negativeScored" }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      });
     }
   },
-  mounted() {
-    this.i18n = this.$i18n;
-    this.i18n.locale = "en";
-    this.initBigChart(0);
+  created() {
+    this.getTagCount();
+    this.getQuestionCount();
+    this.getTop10Tags();
   },
-  beforeDestroy() {
-    this.i18n.locale = "en";
+  mounted() {
   }
 };
 </script>
